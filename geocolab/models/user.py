8580ed5c8a7df6614aaff5ef4a81f618f13ff0e3
Ginger Butcher <alycejenni@gmail.com>
2022-07-06 12:54:21 +0100

    fix: remove country of residence from user data

--- geocolab/models/user.py
+++ geocolab/models/user.py
@@ -12,7 +12,6 @@ class User(db.Model):
     given_name = db.Column(db.String(100), nullable=False)
     given_name_first = db.Column(db.Boolean, nullable=False, server_default='1')
     created = db.Column(db.DateTime(), server_default=func.now())
-    country = db.Column(countries_enum)
     role = db.Column(user_types_enum, server_default='basic', nullable=False)
     pronouns = db.Column(db.String(50))
 
