0c9c3d390ef4c0afdfd6c0d4f3189c96bb131fe8
Ginger Butcher <alycejenni@gmail.com>
2022-07-22 13:54:53 +0100

    fix: get user correctly for admin decorator

--- geocolab/routes/_decorators.py
+++ geocolab/routes/_decorators.py
@@ -3,6 +3,7 @@
 from functools import wraps
 from flask_jwt_extended import verify_jwt_in_request, get_jwt, get_jwt_identity
 from flask import jsonify
+from geocolab.models import User
 
 
 def admin_required(fn):
@@ -22,7 +23,8 @@ def manager_required(org=None):
         @wraps(fn)
         def decorator(*args, **kwargs):
             verify_jwt_in_request()
-            user = get_jwt_identity()
+            user_id = get_jwt_identity()
+            user = User.query.get(user_id)
             access = False
             if org:
                 access = org.can_edit(user)
